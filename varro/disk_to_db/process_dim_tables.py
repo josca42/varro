bd30a9677d0f47a88e203d0c60de11a5183ee38f
josca42 <jonathanscharff@gmail.com>
2025-11-06 13:50:52 +0000

    updates

--- varro/disk_to_db/process_dim_tables.py
+++ varro/disk_to_db/process_dim_tables.py
@@ -2,10 +2,21 @@ import pandas as pd
 
 
 def process_kode_col(df: pd.DataFrame) -> pd.DataFrame:
-    if df["KODE"].dtype == "object":
-        n_unique = df["KODE"].nunique()
-        kode_as_int = df["KODE"].str.replace(".", "").astype(int)
+    if df["kode"].dtype == "object":
+        n_unique = df["kode"].nunique()
+        kode_as_int = df["kode"].str.replace(".", "").astype(int)
         if n_unique == kode_as_int.nunique():
-            df["KODE"] = kode_as_int
+            df["kode"] = kode_as_int
 
     return df
+
+
+def check_if_dim_fits_fact_col(df_fact, df_dim, fact_col):
+    diff = set(df_fact[fact_col].unique()) - set(df_dim["kode"].values)
+    if diff == set():
+        return df_fact
+    elif diff == {0}:
+        df_fact[df_fact[fact_col] != 0]
+        return df_fact
+    else:
+        raise ValueError(f"Diff: {diff}")
